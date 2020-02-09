7544eb8905c8654b068c33f531173cd61cfba146
Kyuunex <kyuunex@protonmail.ch>
2020-02-09 19:24:41 +0400

    don't use db.py in permissions.py

--- modules/permissions.py
+++ modules/permissions.py
@@ -1,13 +1,21 @@
 import discord
-from modules import db
+from modules.connections import database_file as database_file
+import sqlite3
+
+conn = sqlite3.connect(database_file)
+c = conn.cursor()
+db_admin_list = tuple(c.execute("SELECT user_id FROM admins"))
+db_owner_list = tuple(c.execute("SELECT user_id FROM admins WHERE permissions = ?", [str(1)]))
+conn.commit()
+conn.close()
 
 admin_list = []
-for admin_id in db.query("SELECT user_id FROM admins"):
+for admin_id in db_admin_list:
     admin_list.append(admin_id[0])
 
 
 owner_list = []
-for owner_id in db.query(["SELECT user_id FROM admins WHERE permissions = ?", [str(1)]]):
+for owner_id in db_owner_list:
     owner_list.append(owner_id[0])
 
 
