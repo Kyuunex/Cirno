12b1fb1bd180174e3873e39b63715ff02fbb070e
Kyuunex <kyuunex@protonmail.ch>
2021-07-17 03:04:24 +0400

    fix

--- cirno/modules/connections.py
+++ cirno/modules/connections.py
@@ -1,12 +1,12 @@
 import os
-from cirno.modules.storage_management import dirs
+from cirno.modules.storage_management import BOT_DATA_DIR
 
 
 if os.environ.get('CIRNO_TOKEN'):
     bot_token = os.environ.get('CIRNO_TOKEN')
 else:
     try:
-        with open(dirs.user_data_dir + "/token.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/token.txt", "r+") as token_file:
             bot_token = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a bot token. either set CIRNO_TOKEN environment variable")
@@ -17,7 +17,7 @@ if os.environ.get('CIRNO_OSU_API_KEY'):
     osu_api_key = os.environ.get('CIRNO_OSU_API_KEY')
 else:
     try:
-        with open(dirs.user_data_dir + "/osu_api_key.txt", "r+") as token_file:
+        with open(BOT_DATA_DIR + "/osu_api_key.txt", "r+") as token_file:
             osu_api_key = token_file.read().strip()
     except FileNotFoundError as e:
         print("i need a osu api key. either set CIRNO_OSU_API_KEY environment variable")
